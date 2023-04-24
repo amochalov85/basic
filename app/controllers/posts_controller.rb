23a686fbc8e11c7bb64cb99a1b5845f88da6aa4e
root <root@DESKTOP-19V8HBC.localdomain>
2023-04-24 10:47:32 +0300

    Add CRUD for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,4 +21,32 @@ class PostsController < ApplicationController
     @posts.dislikes.create if @post
     redirect_to @posts
   end
+
+  def create
+    Post.create(
+        text: params[:posts][:text],
+        user: current_user)
+    redirect_to users_cabinet_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.user == current_user
+      @post.delete
+      redirect_to users_cabinet_path, notice: 'Post has been deleted'
+    else
+      redirect_to root_path, alert: 'You are not authorized to delete this post'
+    end
+  end
+
+def edit
+    puts(params)
+    @post = Post.find(params[:id])
+end
+
+def update
+    @post = Post.find(params[:id])
+    @post.update(text: params[:post][:text])
+    redirect_to users_cabinet_path
+end
 end
